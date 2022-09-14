2add7285349bfde47471fe5789af9b1ac3a44dd0
Svetlana <bulatova44@gmail.com>
2022-09-14 08:44:00 +0300

    Added PrintNewArray method

--- Answer/Program.cs
+++ Answer/Program.cs
@@ -39,3 +39,15 @@ void GenerateNewArray(string [] array, int count)
     }       
 }
 
+
+void PrintNewArray(string [] array, int count)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= count)
+        {
+            System.Console.Write($"\"{array[i]}\", ");    
+        }
+    }   
+    System.Console.WriteLine();    
+}
